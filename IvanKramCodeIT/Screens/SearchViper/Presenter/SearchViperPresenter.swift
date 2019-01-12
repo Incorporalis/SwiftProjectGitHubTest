cdddd2bb3fd77b8be24cc11f92c630203f87d58a
Ivan.Kramarenko <ivan.kramaren@gmail.com>
2019-01-12 13:28:04 +0200

    [FEAT] (Viper): added viper module for the task

---
+++ IvanKramCodeIT/Screens/SearchViper/Presenter/SearchViperPresenter.swift
@@ -0,0 +1,69 @@
+//
+//  SearchViperPresenter.swift
+//  IvanKramCodeIT
+//
+//  Created by Ivan on 12/01/2019.
+//  Copyright © 2019 IvanKram. All rights reserved.
+//
+
+protocol ISearchViperModuleInput {
+	
+}
+
+class SearchViperPresenter: ISearchViperModuleInput, ISearchViperViewOutput, ISearchViperInteractorOutput {
+
+    weak var view: ISearchViperViewInput?
+    let interactor: ISearchViperInteractorInput
+    let router: ISearchViperRouterInput
+    var elements: [IRepositoryCellViewModel]!
+
+
+    init(with interactor: ISearchViperInteractorInput, router: ISearchViperRouterInput) {
+        self.interactor = interactor
+        self.router = router
+    }
+
+    func configure(with view: ISearchViperViewInput) {
+        self.view = view
+    }
+
+    func viewIsReady() {
+        loadRequest()
+    }
+
+    private func loadRequest() {
+        view?.showHUD()
+
+        loadData()
+    }
+
+    func refreshData() {
+        loadData()
+    }
+
+    func loadData() {
+        interactor.startDataLoading()
+    }
+
+    func didLoadData(with repositories:[IRepository]?) {
+        if let reps = repositories {
+            elements = reps.map { RepositoryCellViewModel(with: $0) }
+            view?.hideHUD()
+            view?.refreshUI()
+        }
+    }
+
+    func didFailLoadData(with error: Error) {
+        self.view?.showFailureRequestAlert(with: error.localizedDescription, actionHandler:{ [weak self] in
+            self?.interactor.startDataLoading()
+            }, completion: { [weak self] in
+                self?.view?.hideHUD()
+        })
+    }
+
+    func viewDidPressDetailsButton(with index: Int) {
+        let models: [Any] = [index]
+		router.showDetails(for: models[index])
+    }
+
+}
